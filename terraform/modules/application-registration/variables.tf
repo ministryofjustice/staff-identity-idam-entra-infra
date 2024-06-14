79b88be69c31966176f8ae762f57640cec5130ff
John Nolan <john.nolan@justice.gov.uk>
2024-06-14 16:33:31 +0100

    Add group_membership_claims and descriptions

--- terraform/modules/application-registration/variables.tf
+++ terraform/modules/application-registration/variables.tf
@@ -1,9 +1,11 @@
 variable "display_name" {
 	type  = string
+    description = "Name of your application. This should be lowercase, alphanumeric in the following pattern: <organisation>-<team>-<applicationname>"
 }
 
 variable "owners" {
 	type  = list
+    description = "UUIDs of the Owners. Should be a minimum of 2 people."
 }
 
 variable "homepage_url" {
@@ -17,3 +19,9 @@ variable "logout_url" {
 variable "redirect_uris" {
 	type  = list
 }
+
+variable "group_membership_claims" {
+	type  = list
+    description = "Optionally send the users group memberships with the claim. Options are None, SecurityGroup, DirectoryRole, ApplicationGroup or All"
+}
+
