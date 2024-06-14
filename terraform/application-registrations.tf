325854d0088da418d2e90d3d52f9c5bfb556a23e
John Nolan <john.nolan@justice.gov.uk>
2024-06-14 16:48:52 +0100

    Add naming and owner validation

--- terraform/application-registrations.tf
+++ terraform/application-registrations.tf
@@ -2,7 +2,7 @@ locals {
   app_registration_list = {
     "eucs-idam-tf-test-app-reg" = {
       display_name  = "eucs-idam-tf-test-app-reg"
-      owners        = ["02acc612-0cc1-4231-a44d-b1bbfcd132f7"]
+      owners        = ["02acc612-0cc1-4231-a44d-b1bbfcd132f7", "02acc612-0cc1-4231-a44d-b1bbfcd132f7"]
       homepage_url  = "https://example.com"
       logout_url    = "https://example.com/logout"
       redirect_uris = ["https://example.com/account"]
@@ -10,7 +10,7 @@ locals {
     },
     "eucs-idam-tf-test-app-reg2" = {
       display_name            = "eucs-idam-tf-test-app-reg2"
-      owners                  = ["02acc612-0cc1-4231-a44d-b1bbfcd132f7"]
+      owners                  = ["02acc612-0cc1-4231-a44d-b1bbfcd132f7", "02acc612-0cc1-4231-a44d-b1bbfcd132f7"]
       homepage_url            = "https://example.com"
       logout_url              = "https://example.com/logout"
       redirect_uris           = ["https://example.com/account"]
--- terraform/modules/application-registration/variables.tf
+++ terraform/modules/application-registration/variables.tf
@@ -1,11 +1,21 @@
 variable "display_name" {
 	type  = string
-    description = "Name of your application. This should be lowercase, alphanumeric in the following pattern: <organisation>-<team>-<applicationname>"
+    description = "Name of your application. This should be at least 8 characters long, lowercase, alphanumeric in the following pattern: organisation-team-applicationname"
+
+    validation {
+        condition     = length(var.display_name) > 8 && can(regex("^[a-z0-9\\-]+$", var.display_name))
+        error_message = "Application Name must be at least 8 characters long, lowercase, alphanumeric in the following pattern: organisation-team-applicationname."
+    }
 }
 
 variable "owners" {
 	type  = list
     description = "UUIDs of the Owners. Should be a minimum of 2 people."
+
+    validation {
+        condition     = length(var.owners) > 1
+        error_message = "This application requires at least two Owners."
+    }
 }
 
 variable "homepage_url" {
