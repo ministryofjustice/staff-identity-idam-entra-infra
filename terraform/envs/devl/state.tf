a4d6a0c9975e2edcbe47d5c168a5b3aa5b9311f7
gillettmoj <jason.gillett@digital.justice.gov.uk>
2025-01-16 10:52:10 +0000

    Adding backend back to versions

--- terraform/envs/devl/state.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-    backend "azurerm" {
-    resource_group_name  = "rg-eucs-idam-observability"
-    storage_account_name = "steucsidamentratf"
-    container_name       = "tfstate-env"
-    key                  = "terraform.tfstate"
-  }
-}
