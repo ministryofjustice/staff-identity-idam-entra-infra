0f1ba564edf36081f107d82e076e51f0c82a0473
jasegillettmoj <jason.gillett@justice.gov.uk>
2025-01-17 12:44:13 +0000

    Adding script to detect changes to envs

---
+++ scripts/pipeline/detect_changes.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+enviroments=("devl" "nle" "live")
+envs_folder="./terraform/envs"
+
+# fetch the list of changed files compared to the default branch
+change_files=$(git diff --name-only origin/main)
+
+for env in "$(environments[@])"; do
+  folder_path="$envs_folder/$env/"
+
+  # check for changed files within the current env dir
+  if echo "$changed_files" | grep -q "^folder_path"; then
+    echo "ENV_ENVIRONMENT_NAME"=$env >> $GITHUB_ENV
+    echo "ENV_ENVIRONMENT_PATH"=$folder_path >> $GITHUB_ENV
+
+    echo "Changes detected in: $env ($folder_path)"
+    exit 0
+  fi
+done
+
+# output if no changes are detected
+echo "No changes detected in any Environment"
+exit 1
