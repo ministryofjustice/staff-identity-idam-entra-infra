4d0049d2437a01d74d531c9837f49a0d5a710c52
jasegillettmoj <jason.gillett@justice.gov.uk>
2025-01-21 10:48:26 +0000

    Workflow rework (#56)
    * Update pr workflow order
    
    * Change workflow rules
    
    * Adding needs to job
    
    * Change the name of the job
    
    * Change the name of the job
    
    * format validate code job
    
    * Adding script to detect changes to envs
    
    * Update pr workflow
    
    * Update detect changes job
    
    * Update validate code job
    
    * Update outputs for detect changes
    
    * Update outputs
    
    * Update terraform plan
    
    * Update validate job
    
    * rm whitespace
    
    * Update action
    
    * Update input var
    
    * Update diff command in script
    
    * remove unneeded fi from script
    
    * Fix typo
    
    * typo
    
    * Update validate job
    
    * comment out tf lint
    
    * Update diff cmd
    
    * update diff cmd
    
    * Update checkout action
    
    * remove fetch depth
    
    * Update script
    
    * update script
    
    * Update script
    
    * Update detect script
    
    * Add test file
    
    * Update workflow pr
    
    * Update test file
    
    * Change detect debug
    
    * Update script
    
    * Update test file
    
    * Update script path output
    
    * Update script path output
    
    * Update test file
    
    * Update env path
    
    * Update test file
    
    * fix typo
    
    * update test file
    
    * Update paths in bash script
    
    * Update jobs with inputs
    
    * update test file
    
    * Change detect changes strategy
    
    * Update detect changes job
    
    * Update matrix
    
    * Update env input

---
+++ scripts/pipeline/detect_changes.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+enviroments=("devl" "nle" "live")
+envs_folder="./terraform/envs"
+
+default_branch="main"
+
+merge_base=$(git merge-base HEAD origin/$default_branch)
+
+for env in "${environments[@]}"; do
+  folder_path="$envs_folder/$env"
+
+  # check for changed files within the current env dir
+  if git diff main -- $folder_path ; then
+    continue
+  fi
+    echo "ENV_ENVIRONMENT_NAME"=$env >> $GITHUB_ENV
+    echo "ENV_ENVIRONMENT_PATH"=$folder_path >> $GITHUB_ENV
+
+    echo "::set-output name=env_environment_name::$env"
+    echo "::set-output name=env_envrionment_path::$folder_path"
+
+    echo "Changes detected in: $env ($folder_path)"
+    exit 0
+done
+
+# output if no changes are detected
+echo "No changes detected in any Environment"
+exit 1
