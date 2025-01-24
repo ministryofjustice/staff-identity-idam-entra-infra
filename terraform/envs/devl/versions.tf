ac695df2b919846175f1550ef1da3457d792c3cd
jasegillettmoj <jason.gillett@justice.gov.uk>
2025-01-24 08:14:21 +0000

    Commenting out provider var

--- terraform/envs/devl/versions.tf
+++ terraform/envs/devl/versions.tf
@@ -7,9 +7,9 @@ provider "azurerm" {
 }
 
 provider "azuread" {
-  tenant_id     = var.tenant_id
-  client_id     = var.client_id
-  client_secret = var.client_secret
+  # tenant_id     = var.tenant_id
+  # client_id     = var.client_id
+  # client_secret = var.client_secret
 }
 
 terraform {
