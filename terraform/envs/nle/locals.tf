824d37d69c0afa0779c630b1ad392344aa9ae948
jasegillettmoj <jason.gillett@justice.gov.uk>
2025-01-28 09:20:38 +0000

    Pr workflow actions update (#59)
    * update on
    
    * Update secret names
    
    * Adding environment to workflow
    
    * Update azure cli ver
    
    * Add in actions
    
    * Remove sub
    
    * Add in init
    
    * Add in wd
    
    * update action
    
    * Comment out cli
    
    * Update oidc
    
    * Adding vars for creds
    
    * Update vars
    
    * Update secrets
    
    * Update versions
    
    * Update secrets in env
    
    * Update storage account name
    
    * Update secrets
    
    * Update env vars
    
    * Update secrets
    
    * Testing env vars
    
    * Remove comments
    
    * Testing env vars
    
    * Removing comments
    
    * Commenting out provider var
    
    * Commenting out provider vars
    
    * Remove comments for provider vars
    
    * Remove unneeded vars
    
    * Add flag to tf init
    
    * Adding Terraform cmds
    
    * Testing azure login action
    
    * Remove azure login action
    
    * Adding in pre commit hooks (#58)
    
    * Adding nle setup
    
    * Testing nle
    
    * set nle dir
    
    * Update backend
    
    * Remove reconfigure
    
    * Update plan for nle
    
    * Switch to devl
    
    * Update strategy
    
    * Update matrix
    
    * Adding live
    
    * Adding live tf config
    
    * rm workflow from tf live dir
    
    * comment out code
    
    * update state live
    
    * Update path to live
    
    * Update path to live
    
    * Update pr
    
    * remove secret
    
    * update pr workflow
    
    * Update authentication
    
    * Update action with
    
    * rm azure login action
    
    * Add in azure login
    
    * Update path to live with oidc auth
    
    * update pre commit
    
    * Update pre commit

---
+++ terraform/envs/nle/locals.tf
@@ -0,0 +1,7 @@
+locals {
+  tags = {
+    department = "eucs"
+    team       = "idam"
+    source     = "terraform"
+  }
+}
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,49 +1,49 @@
-locals {
-  app_registrations = { for s in local.app_registration_list : s.display_name => s }
-}
+# locals {
+#   app_registrations = { for s in local.app_registration_list : s.display_name => s }
+# }
 
-module "application-registration" {
-  source = "./modules/application-registration"
+# module "application-registration" {
+#   source = "./modules/application-registration"
 
-  for_each = { for s, v in local.app_registrations : s => v if contains(v.tenants_required, terraform.workspace) }
+#   for_each = { for s, v in local.app_registrations : s => v if contains(v.tenants_required, terraform.workspace) }
 
-  display_name                 = each.value.display_name
-  department_name              = each.value.department_name
-  team_name                    = each.value.team_name
-  application_name             = each.value.application_name
-  create_access_package        = lookup(each.value, "create_access_package", false)
-  service_management_reference = each.value.service_management_reference
-  notes                        = each.value.notes
-  owners                       = terraform.workspace == "LIVE" ? each.value.owners_live : terraform.workspace == "NLE" ? lookup(each.value, "owners_nle", []) : lookup(each.value, "owners_devl", [])
-  homepage_url                 = each.value.homepage_url
-  logout_url                   = each.value.logout_url
-  redirect_uris = lookup(each.value, "redirect_uris", {
-    devl = null
-    nle  = null
-    live = null
-  })
-  identifier_uris = lookup(each.value, "identifier_uris", {
-    devl = null
-    nle  = null
-    live = null
-  })
-  app_roles                     = lookup(each.value, "app_roles", [])
-  graph_delegated_permissions   = lookup(each.value, "graph_delegated_permissions", [])
-  graph_application_permissions = lookup(each.value, "graph_application_permissions", [])
-  allowed_groups                = lookup(each.value, "allowed_groups", [])
-  access_package_reviewers = lookup(each.value, "access_package_reviewers", {
-    devl = []
-    nle  = []
-    live = []
-  })
-  federated_identity_credentials = lookup(each.value, "federated_identity_credentials", [])
-  service_principle = lookup(each.value, "service_principle", {
-    login_url                     = lookup(each.value, "login_url", null),
-    notification_email_addresses  = lookup(each.value, "notification_email_addresses", []),
-    preferred_single_sign_on_mode = lookup(each.value, "preferred_single_sign_on_mode", null),
-    app_role_assignment_required  = lookup(each.value, "app_role_assignment_required", true)
-    account_enabled               = lookup(each.value, "account_enabled", true),
-    application_template_name     = lookup(each.value, "application_template_name", null),
-    hide                          = lookup(each.value, "hide", null)
-  })
-}
+#   display_name                 = each.value.display_name
+#   department_name              = each.value.department_name
+#   team_name                    = each.value.team_name
+#   application_name             = each.value.application_name
+#   create_access_package        = lookup(each.value, "create_access_package", false)
+#   service_management_reference = each.value.service_management_reference
+#   notes                        = each.value.notes
+#   owners                       = terraform.workspace == "LIVE" ? each.value.owners_live : terraform.workspace == "NLE" ? lookup(each.value, "owners_nle", []) : lookup(each.value, "owners_devl", [])
+#   homepage_url                 = each.value.homepage_url
+#   logout_url                   = each.value.logout_url
+#   redirect_uris = lookup(each.value, "redirect_uris", {
+#     devl = null
+#     nle  = null
+#     live = null
+#   })
+#   identifier_uris = lookup(each.value, "identifier_uris", {
+#     devl = null
+#     nle  = null
+#     live = null
+#   })
+#   app_roles                     = lookup(each.value, "app_roles", [])
+#   graph_delegated_permissions   = lookup(each.value, "graph_delegated_permissions", [])
+#   graph_application_permissions = lookup(each.value, "graph_application_permissions", [])
+#   allowed_groups                = lookup(each.value, "allowed_groups", [])
+#   access_package_reviewers = lookup(each.value, "access_package_reviewers", {
+#     devl = []
+#     nle  = []
+#     live = []
+#   })
+#   federated_identity_credentials = lookup(each.value, "federated_identity_credentials", [])
+#   service_principle = lookup(each.value, "service_principle", {
+#     login_url                     = lookup(each.value, "login_url", null),
+#     notification_email_addresses  = lookup(each.value, "notification_email_addresses", []),
+#     preferred_single_sign_on_mode = lookup(each.value, "preferred_single_sign_on_mode", null),
+#     app_role_assignment_required  = lookup(each.value, "app_role_assignment_required", true)
+#     account_enabled               = lookup(each.value, "account_enabled", true),
+#     application_template_name     = lookup(each.value, "application_template_name", null),
+#     hide                          = lookup(each.value, "hide", null)
+#   })
+# }
