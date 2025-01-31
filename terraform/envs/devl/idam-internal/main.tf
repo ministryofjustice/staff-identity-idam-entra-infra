62f965df366ccddd75424ee87a59245ca3326e0e
jasegillettmoj <jason.gillett@justice.gov.uk>
2025-01-31 08:04:50 +0000

    Move files to idam-internal

--- terraform/envs/devl/idam-internal/main.tf
+++ terraform/envs/devl/idam-internal/main.tf
@@ -1,3 +1,11 @@
+locals {
+  tags = {
+    department = "eucs"
+    team       = "idam"
+    source     = "terraform"
+  }
+}
+
 resource "azuread_group" "tf_test" {
   display_name     = "Terraform-Test-Deployment"
   security_enabled = true
