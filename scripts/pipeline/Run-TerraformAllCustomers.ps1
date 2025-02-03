955525fa00cc089768636eb7aecc20aaa03fcad0
jasegillettmoj <jason.gillett@justice.gov.uk>
2025-02-03 14:53:36 +0000

    Add child dir script

---
+++ scripts/pipeline/Run-TerraformAllCustomers.ps1
@@ -0,0 +1,25 @@
+[CmdletBinding()]
+param (
+    [ValidateNotNullOrEmpty()]
+    [string]$Env,
+
+    [ValidateNotNullOrEmpty()]
+    [string]$TerraformCommand
+)
+
+# Global Vars
+$env = $Env.ToLower()
+$baseDir = "terraform/envs/$Env"
+
+# Get all customers dirs per env
+Set-Location $baseDir
+$customers = Get-ChildItem -Directory | Select-Object -ExpandProperty Name
+
+# Run a Terraform command for each customer
+foreach ($customer in $customers) {
+    $workingDir = "$baseDir/$customer"
+    Set-Location -Path $workingDir
+    Write-Host "Running terraform $TerraformCommand for: [$customer]"
+    terraform $TerraformCommand
+    Set-Location $PSScriptRoot
+}
