0ca17ff37da9d8e6197d7cdf617c58852977bf3d
jasegillettmoj <jason.gillett@justice.gov.uk>
2025-02-04 10:22:59 +0000

    Update path to live

--- scripts/pipeline/Run-TerraformAllCustomers.ps1
+++ scripts/pipeline/Run-TerraformAllCustomers.ps1
@@ -4,26 +4,36 @@ param (
     [string]$Env,
 
     [ValidateNotNullOrEmpty()]
-    [string]$TerraformCommand
+    [string]$TerraformCommand,
+
+    [switch]$TerraformPlanAndApply
 )
+try {
+    # Global Vars
+    $env = $Env.ToLower()
+    $baseDir = "./terraform/envs/$env"
 
-# Global Vars
-$env = $Env.ToLower()
-$baseDir = "./terraform/envs/$env"
-$command = "terraform $TerraformCommand"
+    if ($TerraformPlanAndApply) {
+        $command = "terraform apply -lock-timeout=300s -input=false -auto-approve -parallelism=30"
+    } else {
+        $command = "terraform $TerraformCommand"
+    }
 
-# Get all customers dirs per env
-Set-Location $baseDir
-Write-Host $(Get-Location)
-$customers = Get-ChildItem -Directory | Select-Object -ExpandProperty Name
-Write-Host $customers
+    # Get all customers dirs per env
+    Set-Location $baseDir
+    Write-Host $(Get-Location)
+    $customers = Get-ChildItem -Directory | Select-Object -ExpandProperty Name
+    Write-Host $customers
 
 
-# Run a Terraform command for each customer
-foreach ($customer in $customers) {
-    $workingDir = Resolve-Path -Path "$customer"
-    Set-Location -Path $workingDir
-    Write-Host "Running terraform $TerraformCommand for: [$customer]"
-    Invoke-Expression $command
-    cd ..
+    # Run a Terraform command for each customer
+    foreach ($customer in $customers) {
+        $workingDir = Resolve-Path -Path "$customer"
+        Set-Location -Path $workingDir
+        Write-Host "Running terraform $TerraformCommand for: [$customer]"
+        Invoke-Expression $command
+        Set-Location $baseDir
+    }
+} catch {
+    throw $_
 }
