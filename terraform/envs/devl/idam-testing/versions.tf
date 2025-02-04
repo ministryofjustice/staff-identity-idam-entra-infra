675a05789a8c0c595c43276a93df76e2980cace9
jasegillettmoj <jason.gillett@justice.gov.uk>
2025-02-04 10:29:37 +0000

    Add test folder

---
+++ terraform/envs/devl/idam-testing/versions.tf
@@ -0,0 +1,23 @@
+provider "azurerm" {
+  use_oidc = true
+  features {}
+}
+
+provider "azuread" {
+}
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>4.14"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = ""
+    storage_account_name = ""
+    container_name       = ""
+    key                  = ""
+    use_oidc             = true
+  }
+}
