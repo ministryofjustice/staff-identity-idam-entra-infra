1a924e0523fc629e4c7bd08b6287bdc02462f2d0
jasegillettmoj <jason.gillett@justice.gov.uk>
2025-02-07 10:26:09 +0000

    Testing appreg

--- terraform/envs/devl/idam-testing/versions.tf
+++ terraform/envs/devl/idam-testing/versions.tf
@@ -14,10 +14,10 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name  = ""
-    storage_account_name = ""
-    container_name       = ""
-    key                  = ""
+    resource_group_name  = "rg-eucs-idam-observability"
+    storage_account_name = "stidamobservetfstate"
+    container_name       = "tfstate-env"
+    key                  = "terraform-idam-testing.tfstate"
     use_oidc             = true
   }
 }
