d9e161180fc22ac230ef684dd979c19370efda3a
jasegillettmoj <jason.gillett@justice.gov.uk>
2025-07-02 10:01:04 +0100

    Create app reg

---
+++ terraform/envs/devl/canda/variables.tf
@@ -0,0 +1,147 @@
+variable "location" {
+  description = "Azure region for resources to be deployed to."
+  type        = string
+  default     = "uksouth"
+}
+
+variable "applications" {
+  description = "Map of application details"
+  type = map(object({
+    notes                        = string
+    service_management_reference = string
+    display_name                 = string
+    department_name              = string
+    team_name                    = string
+    application_name             = string
+    create_access_package        = bool
+    access_package_reviewers     = list(string)
+    owners                       = list(string)
+    allowed_groups               = list(string)
+    homepage_url                 = string
+    logout_url                   = string
+    redirect_uris                = list(string)
+    mobile_desktop_redirect_uris = list(string)
+    app_roles = list(object({
+      allowed_member_types  = list(string)
+      description           = string
+      display_name          = string
+      id                    = string
+      value                 = string
+      access_package_hidden = bool
+    }))
+    graph_application_permissions = list(string)
+    graph_delegated_permissions   = list(string)
+    tenants_required              = list(string)
+    federated_identity_credentials = list(object({
+      repo_name      = string
+      display_name   = string
+      description    = string
+      subject_suffix = string
+    }))
+    service_principle = object({
+      login_url                     = string
+      notification_email_addresses  = list(string)
+      preferred_single_sign_on_mode = string
+      app_role_assignment_required  = bool
+      account_enabled               = bool
+      application_template_name     = string
+      hide                          = bool
+    })
+    identifier_uris = list(string)
+  }))
+  default = {
+    "canda_l2" = {
+      notes                          = "Used to grant access to Graph API scopes - Converge And Accelerate Evolve Project Team L2"
+      service_management_reference   = "IDAM-1811"
+      display_name                   = "MOJO-canda-l2-Graph-Permissions"
+      department_name                = "Justice-Digital"
+      team_name                      = "Converge-And-Accelerate-Evolve-Project-Team"
+      application_name               = "MOJO-canda-l2-Graph-Permissions"
+      create_access_package          = false
+      access_package_reviewers       = []
+      owners                         = []      
+      allowed_groups                 = []
+      homepage_url                   = null
+      logout_url                     = null
+      redirect_uris                  = null
+      mobile_desktop_redirect_uris   = null
+      app_roles                      = []
+      graph_application_permissions  = []
+      graph_delegated_permissions    = []
+      tenants_required               = ["DEVL"]
+      federated_identity_credentials = []
+      service_principle = {
+        login_url                     = null
+        notification_email_addresses  = []
+        preferred_single_sign_on_mode = null
+        app_role_assignment_required  = true
+        account_enabled               = true
+        application_template_name     = null
+        hide                          = true
+      }
+      identifier_uris = null
+    },
+    "canda_l3" = {
+      notes                          = "Used to grant access to Graph API scopes - Converge And Accelerate Evolve Project Team L3"
+      service_management_reference   = "IDAM-1811"
+      display_name                   = "MOJO-canda-l3-Graph-Permissions"
+      department_name                = "Justice-Digital"
+      team_name                      = "Converge-And-Accelerate-Evolve-Project-Team"
+      application_name               = "MOJO-canda-l3-Graph-Permissions"
+      create_access_package          = false
+      access_package_reviewers       = []
+      owners                         = []      
+      allowed_groups                 = []
+      homepage_url                   = null
+      logout_url                     = null
+      redirect_uris                  = null
+      mobile_desktop_redirect_uris   = null
+      app_roles                      = []
+      graph_application_permissions  = []
+      graph_delegated_permissions    = []
+      tenants_required               = ["DEVL"]
+      federated_identity_credentials = []
+      service_principle = {
+        login_url                     = null
+        notification_email_addresses  = []
+        preferred_single_sign_on_mode = null
+        app_role_assignment_required  = true
+        account_enabled               = true
+        application_template_name     = null
+        hide                          = true
+      }
+      identifier_uris = null
+    },
+    "canda_Architect" = {
+      notes                          = "Used to grant access to Graph API scopes - Converge And Accelerate Evolve Project Team Architect"
+      service_management_reference   = "IDAM-1811"
+      display_name                   = "MOJO-canda-Architect-Graph-Permissions"
+      department_name                = "Justice-Digital"
+      team_name                      = "Converge-And-Accelerate-Evolve-Project-Team"
+      application_name               = "MOJO-canda-Architect-Graph-Permissions"
+      create_access_package          = false
+      access_package_reviewers       = []
+      owners                         = []      
+      allowed_groups                 = []
+      homepage_url                   = null
+      logout_url                     = null
+      redirect_uris                  = null
+      mobile_desktop_redirect_uris   = null
+      app_roles                      = []
+      graph_application_permissions  = []
+      graph_delegated_permissions    = []
+      tenants_required               = ["DEVL"]
+      federated_identity_credentials = []
+      service_principle = {
+        login_url                     = null
+        notification_email_addresses  = []
+        preferred_single_sign_on_mode = null
+        app_role_assignment_required  = true
+        account_enabled               = true
+        application_template_name     = null
+        hide                          = true
+      }
+      identifier_uris = null
+    }
+  }
+}
