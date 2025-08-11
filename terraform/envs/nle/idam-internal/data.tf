83078133fa09bd7872e8c1f62f824253963ff422
jasegillettmoj <jason.gillett@justice.gov.uk>
2025-08-11 16:01:31 +0100

    Update data call

--- terraform/envs/nle/idam-internal/data.tf
+++ terraform/envs/nle/idam-internal/data.tf
@@ -5,5 +5,5 @@ data "azuread_client_config" "current" {}
 data "azurerm_client_config" "current" {}
 
 data "azurerm_resource_group" "internal-rg" {
-  name = "rg-eucs-idam-observability"
+  name = "rg-eucs-idam-observability-pipeline"
 }
