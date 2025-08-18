9b546bdf6cff108f982604f86d680a0447290ab0
jasegillettmoj <jason.gillett@justice.gov.uk>
2025-08-18 12:51:27 +0100

    Update script

--- scripts/pipeline/Get-Changes.ps1
+++ scripts/pipeline/Get-Changes.ps1
@@ -13,15 +13,18 @@ param (
 $env = $Env.ToLower()
 $baseDir = "./terraform/envs/$env/"
 
+# Set Terraform command based on CICD input
 if ($TerraformPlanAndApply) {
     $command = "terraform apply -lock-timeout=300s -input=false -auto-approve -parallelism=30"
 } else {
     $command = "terraform $TerraformCommand"
 }
 
+# Get git diff of main and feature branch
 $diff = git diff --name-only origin/main
 Write-Host "`e[33m Diff output is [$diff] `e[0m"
 
+# Extract the env and customer name from each changed terraform file, based on the filenames of each file in the git diff
 $changedFiles = $diff | ForEach-Object {
     $path = $_.Split("/")
 
@@ -33,29 +36,32 @@ $changedFiles = $diff | ForEach-Object {
     }
 }
 
+# Filter changed files for unique customer/env combos to remove duplication, which will happen if you update multiple files for a customer
 $uniqueCustomers = $changedFiles | Group-Object {
     "$($_.Customer)|$($_.Env)" } | ForEach-Object {
     $_.Group[0]
 }
 
-Write-Host "`e[33m Change will run for $($uniqueCustomers.Name) `e[0m"
+Write-Host "`e[32m Runnning terraform for Customers $($uniqueCustomers.Name) `e[0m"
 
-Write-Host "`e[32m Runnning terraform for Customers `e[0m"
-
+# Setup array for jobs output
 $jobs = @()
 
 foreach ($customer in $uniqueCustomers) {
+
+    # Due to using GH environments. We need this to skip changed files in one env, triggering a differnet env
     if ($ENV -ne $customer.Env.ToUpper()) {
         Write-Host "⏭️ Skipping $($customer.Name) — environment mismatch ($($customer.Env) vs $ENV)" -ForegroundColor DarkGray
         continue
     }
 
     Write-Host "`e[34m✅ Processing $($customer.Name) in environment $($customer.Env)`e[0m" 
-
     
     $baseDir = "./terraform/envs/$($customer.env)/$($customer.Name)/"
+
     Push-Location $baseDir
 
+    # Setup concurrency for multiple changes to one env
     $jobs += Start-Job -Name $customer.Name -ArgumentList $customer.Name, $command -ScriptBlock {
     param($customerName, $command)
         try {
@@ -73,8 +79,9 @@ foreach ($customer in $uniqueCustomers) {
 # Wait for all jobs to complete
 $jobs | Wait-Job
 
+# Output job output to console
 foreach ($job in $jobs) {
-    Write-Host "Output for Job: $($job.Name)" -ForegroundColor Cyan
+    Write-Host "`e[34m Output for Job: $($job.Name) `e[0m"
     Receive-Job $job
 }
 
