07627a3bc188c47411b868826892f3518eaed1f4
jasegillettmoj <jason.gillett@justice.gov.uk>
2025-08-18 12:56:47 +0100

    Update tf

--- terraform/envs/devl/idam-internal/main.tf
+++ terraform/envs/devl/idam-internal/main.tf
@@ -40,31 +40,3 @@ resource "azurerm_key_vault" "idam-internal_vault" {
     ]
   }
 }
-
-#region Application Registrations
-module "application-registration" {
-  source                         = "../../../modules/application-registrationV1.2.0"
-  for_each                       = var.applications
-  notes                          = each.value.notes
-  service_management_reference   = each.value.service_management_reference
-  display_name                   = each.value.display_name
-  department_name                = each.value.department_name
-  team_name                      = each.value.team_name
-  application_name               = each.value.application_name
-  create_access_package          = each.value.create_access_package
-  access_package_reviewers       = each.value.access_package_reviewers
-  owners                         = each.value.owners
-  allowed_groups                 = each.value.allowed_groups
-  homepage_url                   = each.value.homepage_url
-  logout_url                     = each.value.logout_url
-  redirect_uris                  = each.value.redirect_uris
-  mobile_desktop_redirect_uris   = each.value.mobile_desktop_redirect_uris
-  app_roles                      = each.value.app_roles
-  graph_application_permissions  = each.value.graph_application_permissions
-  graph_delegated_permissions    = each.value.graph_delegated_permissions
-  tenants_required               = each.value.tenants_required
-  federated_identity_credentials = each.value.federated_identity_credentials
-  service_principle              = each.value.service_principle
-  identifier_uris                = each.value.identifier_uris
-}
-#endregion
