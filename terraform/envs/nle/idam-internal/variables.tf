fefeb42ad29cfae0d8a78ddfc5135b1f22fc2e95
Fairfield <lee.fairfield@justice.gov.uk>
2025-11-27 10:07:06 +0000

    adding applicaiton NIAM B2C Federation - NLE

--- terraform/envs/nle/idam-internal/variables.tf
+++ terraform/envs/nle/idam-internal/variables.tf
@@ -18,3 +18,94 @@ variable "engineer_object_ids" {
     "john"    = "38cc9777-479f-450f-acf8-f8856ba861ba"
   }
 }
+
+variable "applications" {
+  description = "Map of application details"
+  type = map(object({
+    notes                        = string
+    service_management_reference = string
+    display_name                 = string
+    department_name              = string
+    team_name                    = string
+    application_name             = string
+    create_access_package        = bool
+    access_package_reviewers     = list(string)
+    owners                       = list(string)
+    allowed_groups               = list(string)
+    homepage_url                 = string
+    logout_url                   = string
+    redirect_uris                = list(string)
+    mobile_desktop_redirect_uris = list(string)
+    app_roles = list(object({
+      allowed_member_types  = list(string)
+      description           = string
+      display_name          = string
+      id                    = string
+      value                 = string
+      access_package_hidden = bool
+    }))
+    graph_application_permissions = list(string)
+    graph_delegated_permissions   = list(string)
+    tenants_required              = list(string)
+    federated_identity_credentials = list(object({
+      repo_name      = string
+      display_name   = string
+      description    = string
+      subject_suffix = string
+    }))
+    service_principle = object({
+      login_url                     = string
+      notification_email_addresses  = list(string)
+      preferred_single_sign_on_mode = string
+      app_role_assignment_required  = bool
+      account_enabled               = bool
+      application_template_name     = string
+      hide                          = bool
+    })
+    identifier_uris = list(string)
+  }))
+  default = {
+    "NIAM B2C Federation - NLE" = {
+      notes                          = "NIAM B2C Federation - NLE"
+      service_management_reference   = "IDAM-4973"
+      logo_image                     = "assets/moj-square-icon-215x215.png"
+      display_name                   = "NIAM B2C Federation - NLE"
+      department_name                = "eucs"
+      team_name                      = "snow"
+      application_name               = "NIAM B2C Federation - NLE"
+      create_access_package          = false
+      access_package_reviewers       = []
+      owners                         = ["James.Irving1@justice.gov.uk"]      
+      application_contacts           = ["James.Irving1@justice.gov.uk"]      
+      allowed_groups                 = []
+      homepage_url                   = null
+      logout_url                     = null
+      redirect_uris                  = ["https://niamppd.b2clogin.com/niamppd.onmicrosoft.com/oauth2/authresp"]
+      mobile_desktop_redirect_uris   = null
+      app_roles                      = []
+      graph_application_permissions  = []
+      graph_delegated_permissions    = ["User.Read"]
+      tenants_required               = ["NLE"]
+      access_token_issuance_enabled  = false
+      id_token_issuance_enabled      = false
+      federated_identity_credentials = []
+      service_principle = {
+        login_url                     = null
+        notification_email_addresses  = []
+        preferred_single_sign_on_mode = null
+        app_role_assignment_required  = false
+        account_enabled               = true
+        application_template_name     = null
+        hide                          = true
+        custom_single_sign_on         = null
+      }
+      identifier_uris = null
+      api = {
+        known_client_applications      = [],
+        mapped_claims_enabled          = false,
+        requested_access_token_version = null,
+        oauth2_permission_scope = []
+      }
+    }
+  }
+}
