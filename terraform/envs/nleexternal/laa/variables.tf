aa6aa08c062118f0a51c999b6232d2155e8be003
Ben Millar <ben.millar@justice.gov.uk>
2026-01-22 11:38:15 +0000

    Grant User.ReadBasic.All delegated permission to recall user's names from UID (#337)
    Co-authored-by: John Nolan <john.nolan@justice.gov.uk>

--- terraform/envs/nleexternal/laa/variables.tf
+++ terraform/envs/nleexternal/laa/variables.tf
@@ -619,7 +619,7 @@ variable "applications" {
       mobile_desktop_redirect_uris   = null
       app_roles                      = []
       graph_application_permissions  = []
-      graph_delegated_permissions    = ["User.Read"]
+      graph_delegated_permissions    = ["User.Read", "User.ReadBasic.All"]
       tenants_required               = ["NLEEXTERNAL"]
       access_token_issuance_enabled  = false
       id_token_issuance_enabled      = false
