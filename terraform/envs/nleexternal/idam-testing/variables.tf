70b49190b8de2b4c5b65e6e13275a17d0f424f3d
John Nolan <john.nolan@justice.gov.uk>
2026-02-06 09:50:17 +0000

    Upgrade idam-testing to latest module version

--- terraform/envs/nleexternal/idam-testing/variables.tf
+++ terraform/envs/nleexternal/idam-testing/variables.tf
@@ -7,20 +7,25 @@ variable "location" {
 variable "applications" {
   description = "Map of application details"
   type = map(object({
-    notes                        = string
-    service_management_reference = string
-    display_name                 = string
-    department_name              = string
-    team_name                    = string
-    application_name             = string
-    create_access_package        = bool
-    access_package_reviewers     = list(string)
-    owners                       = list(string)
-    allowed_groups               = list(string)
-    homepage_url                 = string
-    logout_url                   = string
-    redirect_uris                = list(string)
-    mobile_desktop_redirect_uris = list(string)
+    notes                         = string
+    service_management_reference  = string
+    logo_image                    = string
+    display_name                  = string
+    department_name               = string
+    team_name                     = string
+    application_name              = string
+    create_access_package         = bool
+    access_package_reviewers      = list(string)
+    owners                        = list(string)
+    application_contacts          = list(string)
+    allowed_groups                = list(string)
+    homepage_url                  = string
+    logout_url                    = string
+    redirect_uris                 = list(string)
+    access_token_issuance_enabled = bool
+    id_token_issuance_enabled     = bool
+    mobile_desktop_redirect_uris  = list(string)
+    tags = list(string)
     app_roles = list(object({
       allowed_member_types  = list(string)
       description           = string
@@ -46,13 +51,30 @@ variable "applications" {
       account_enabled               = bool
       application_template_name     = string
       hide                          = bool
+      custom_single_sign_on         = bool
     })
     identifier_uris = list(string)
+    api = object({
+      known_client_applications      = list(string)
+      mapped_claims_enabled          = bool
+      requested_access_token_version = string
+      oauth2_permission_scope = list(object({
+        admin_consent_description  = string
+        admin_consent_display_name = string
+        enabled                    = bool
+        id                         = string
+        type                       = string
+        user_consent_description   = string
+        user_consent_display_name  = string
+        value                      = string
+      }))
+    })
   }))
   default = {
     "app1" = {
       notes                          = "EUCS IDAM App Reg Test 1 terraform"
       service_management_reference   = "IDAM-2974"
+      logo_image                     = "./assets/moj-square-icon-215x215.png"
       display_name                   = "EUCS IDAM App Reg Test 1 terraform"
       department_name                = "eucs"
       team_name                      = "idam"
@@ -69,7 +91,10 @@ variable "applications" {
       graph_application_permissions  = []
       graph_delegated_permissions    = ["User.Read"]
       tenants_required               = ["NLEEXTERNAL"]
+      access_token_issuance_enabled  = false
+      id_token_issuance_enabled      = false
       federated_identity_credentials = []
+      tags = ["Business unit: IDAM"]
       service_principle = {
         login_url                     = null
         notification_email_addresses  = []
@@ -78,8 +103,15 @@ variable "applications" {
         account_enabled               = true
         application_template_name     = null
         hide                          = null
+        custom_single_sign_on         = null
       }
       identifier_uris = null
+      api = {
+        known_client_applications      = null,
+        mapped_claims_enabled          = true,
+        requested_access_token_version = 2,
+        oauth2_permission_scope        = []
+      }
     }
   }
 }
