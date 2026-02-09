04e8ca73584bc21a39128f79eebab16d4b08199e
John Nolan <john.nolan@justice.gov.uk>
2026-02-09 13:22:12 +0000

    Connect `obo_example_frontend` to `obo_example_internal_api`

--- terraform/envs/nleexternal/idam-testing/main.tf
+++ terraform/envs/nleexternal/idam-testing/main.tf
@@ -163,7 +163,15 @@ locals {
       redirect_uris                = ["https://test.justice.gov.uk/auth"]
       mobile_desktop_redirect_uris = null
       app_roles                    = []
-      resource_access = []
+      resource_access              = [
+        {
+          resource_app_name = "obo_internal_api"
+          resource_access = {
+            id   = random_uuid.scope_obo_internal_api.result # Requesting 'user_impersonation'
+            type = "Scope"
+          }
+        }
+      ]
       graph_application_permissions  = []
       graph_delegated_permissions    = ["User.Read"]
       access_token_issuance_enabled  = false
